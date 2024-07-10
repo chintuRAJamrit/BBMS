30c98b4fe1c3db92660cf47cdd859d1da318abec
chintuRAJamrit <133254549+chintuRAJamrit@users.noreply.github.com>
2024-07-10 13:24:52 +0530

    php/MySql project

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Bank Management</title>
+    
+    <link rel="stylesheet" href="bootstrap/css//bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <a class="navbar-brand" href="index.php">Blood Bank Management System</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="admin/login.php">Admin</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="donor/login.php">Donor</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="patient/login.php">Patient</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+
+ <div  class="container-fluid" style="margin:50px;">
+    <div class="row container" style="text-align: justify;">
+        <div class="col-md-8 mx-auto content-left">
+            <h4>Our Project</h4>
+            <p>
+            This is our project on the Blood Donation Management System, completed as part of our 5th-semester coursework. Throughout this project, we encountered various technical challenges, such as configuring our server from port 80 to 8080 while running Apache. We also gained valuable experience working with PHP and phpMyAdmin for database management, allowing us to view and edit our database effectively. </p>
+
+           <p> In addition to backend development, we enhanced our skills in frontend technologies, including HTML, CSS, Bootstrap, JavaScript, and jQuery, which we used to create a user-friendly interface. A significant part of our learning involved using MySQL for database management, which played a crucial role in ensuring data consistency and reliability. This project not only deepened our understanding of web development but also provided us with practical experience in integrating various technologies to create a comprehensive system. </p>
+        </div>
+        <div class="col-md-3 content-right">
+            <img class="img-fluid" src="../BBMS/images/image1.png" height="auto" width="110%">
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10 m-auto" style="text-align: justify;">
+            <h4>What is Blood Bank Management System?</h4>
+            <p>
+            A Blood Bank Management System (BBMS) is a digital solution designed to streamline the operations of blood banks. It facilitates efficient management of donor records, blood inventory, and distribution processes, ensuring timely availability of blood for patients. The system includes features for donor registration, blood collection scheduling, and inventory tracking. It also manages blood testing, labeling, and storage information, enhancing traceability and safety.            </p>
+            <p>
+            Additionally, a BBMS helps in managing blood requests and supply, ensuring compatibility checks, and coordinating with hospitals for timely delivery. It often includes analytics for monitoring blood stock levels, predicting shortages, and generating reports. This system supports compliance with regulatory standards and improves communication between blood banks and healthcare providers. By automating and optimizing various processes, BBMS enhances the overall efficiency and reliability of blood bank operations, ultimately contributing to better patient care and saving lives.            </p>
+        </div>
+    </div>
+</div>
+<div class="container-fluid" style="margin-bottom: 5%;">
+    <div class="row">
+        <div class="col-10  m-auto">
+        <h4>Our Team</h4>
+        <div class="card container">
+            <h4>Abhiram U</h4>
+            <hr>
+            <p> UO3NM21T064003 </p>
+            <p> ISE 5TH SEM </p>
+            <p> CSE DEPT, UVCE </p>
+            </div>
+        <div class="card">
+            <h4>Amrit Raj</h4>
+            <hr>
+            <p> UO3NM21T064004 </p>
+            <p> ISE 5TH SEM </p>
+            <p> CSE DEPT, UVCE </p>
+            
+        </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 bg-danger footer">
+            &copy 2024 DBMS PROJECT
+        </div>
+    </div>
+</div>
+</body>
+</html>
